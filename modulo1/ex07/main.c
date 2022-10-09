e4f441a3014b8f8d7b481d34b002c9249fb9f8c8
LuisMonteiro <1211250@isep.ipp.pt>
2022-10-09 12:21:03 +0100

    erro no main

--- modulo1/ex07/main.c
+++ modulo1/ex07/main.c
@@ -4,7 +4,7 @@
 int main(){
 int array[6]={1,4,2,6,3,-1};
 int *point = array;
-array_sort1sort(point,6);
+array_sort1(point,6);
 // Dar print aos valores
 for (int i = 0; i<6;i++){
 printf("%d \n",*(array + i));
