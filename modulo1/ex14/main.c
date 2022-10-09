0e2332cf8e43f2f153519d58bd2a97bec51781d2
Pedro <1211171@isep.ipp.pt>
2022-10-09 22:37:27 +0100

    exercicio 14

---
+++ modulo1/ex14/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "frequencies.h"
+
+int main(int ac, char** av){
+	float grades[]={8.23, 12.25, 16.45, 12.45, 10.05, 6.45, 14.45, 0.0, 12.67,16.23, 18.75};     //Instanciar variaveis notas, numero de alunos e criar um array com a frequencia de cada nota
+	int n=11;
+	int frequency[n];
+	int i;
+	
+	frequencies(grades, n, frequency); //Chamar metodo
+	
+	for(i=0; i<n;i++){
+			printf("%d\n", frequency[i]); //Imprimir resultado
+	}
+}													// +/- 45 minutos porque demorou p perceber o enunciado
