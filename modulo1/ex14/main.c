99df0cf65f6ddaee065f40d26cb7fcde88e6b48c
Pedro <1211171@isep.ipp.pt>
2022-10-09 23:51:26 +0100

    updates to 14

--- modulo1/ex14/main.c
+++ modulo1/ex14/main.c
@@ -3,7 +3,7 @@
 
 int main(int ac, char** av){
 	float grades[]={8.23, 12.25, 16.45, 12.45, 10.05, 6.45, 14.45, 0.0, 12.67,16.23, 18.75};     //Instanciar variaveis notas, numero de alunos e criar um array com a frequencia de cada nota
-	int n=11;
+	int n=20;
 	int frequency[n];
 	int i;
 	
